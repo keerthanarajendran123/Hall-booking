ef8d13985c322d3ef612c235081c77bace0ad5c4
keerthanarajendran123 <keerthurb9@gmail.com>
2023-12-12 11:45:00 +0530

    h1 tag updated

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import connectDB from './src/db/index.js'; 
-// import roomRoutes from './src/routes/roomRoutes.js';
-// import bookingRoutes from './src/routes/bookingRoutes.js';
-
-// dotenv.config();
-// const app = express();
-
-// app.use(express.json());
-
-// connectDB();
-
-// app.use('/rooms', roomRoutes);
-// app.use('/bookings', bookingRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
-// export default app;
-
-
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/db/index.js'; 
@@ -31,16 +7,14 @@ import bookingRoutes from './src/routes/bookingRoutes.js';
 dotenv.config();
 const app = express();
 
-// Middleware to add a welcome message
-app.use((req, res, next) => {
-    res.header('Content-Type', 'text/html');
-    res.write('<h1>Welcome to Hall Booking App</h1>');
-    next();
-});
-
 app.use(express.json());
 
 connectDB();
+
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Hall Booking App</h1>');
+});
 
 app.use('/rooms', roomRoutes);
 app.use('/bookings', bookingRoutes);
